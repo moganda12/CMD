1b96efe4d4e1f4b133dfd1cc8291a8a9ef6f7edf
moganda12 <115957608+moganda12@users.noreply.github.com>
2024-08-06 23:43:59 +0000

    0.0.1 made the game in a weekend

--- Transistor.cpp
+++ Transistor.cpp
@@ -39,7 +39,10 @@ enum Buildings {
     moss = 7569504340697116271,
     smallFAB = -2000947298636880309,
     mediumFAB = -1712167728928864321,
-    largeFAB = -7940484003211731860
+    largeFAB = -7940484003211731860,
+    i860 = -3460087220543975172,
+    startup = 9083628158375851998,
+    oaktree = 6580827420282859339
 };
 
 struct TclickerSave {
@@ -49,17 +52,17 @@ struct TclickerSave {
 	number heavenlyMicrochips;*/
 	integer clicks; 
     integer cusors, moss, smallFABs, mediumFABs, largeFABs; 
-	//integer i860s, startups;
+	//integer i860s, startups, oakTrees;
 };
 
 TclickerSave save = {0, 0, 0, 0, 0, 0, 0, 0};
 
-str name = "Transistor Clicker";
-str version = "0.0.1 DevBuild 4.1_1";
+const str name = "Transistor Clicker";
+const str version = "0.0.1 DevBuild 4.2_2";
 
-number cursorPrice = 15, mossPrice = 100, smallFABPrice = 1'000, mediumFABprice = 11'000, largeFABPrice = 120'000, intelI860Price = 1'3;
-number cursorYeild = 0.1, mossYeild = 1, smallFABYeild = 10, mediumFABYeild = 60, largeFABYeild = 260;
-number expantionFactor = number(23, 20);
+const number cursorPrice = 15, mossPrice = 100, smallFABPrice = 1'000, mediumFABprice = 11'000, largeFABPrice = 120'000, intelI860Price = 1'305'078, startupPrice = 17'000'000, oakTreePrice = 200'000'000;
+const number cursorYeild = 0.1, mossYeild = 1, smallFABYeild = 10, mediumFABYeild = 60, largeFABYeild = 260, intelI860Yeild = 1'700, startupYeild = 10'000, oakTreeYeild = 120'000;
+const number expantionFactor = number(23, 20);
 
 integer toInt(number x) {
 	return integer(x);
@@ -94,10 +97,13 @@ void clear(std::vector<str>&) {
 	printTitileCard();
 }
 
-str TransitorsString(number transistors, str colA = BOLDGREEN, str colB = BOLDBLUE) {
+str TransitorsString(number transistors, str colA = BOLDGREEN, str colB = BOLDBLUE, integer precision = 1) {
 	std::stringstream ss;
-	str plural = transistors == 1 ? "transistor" : "transistors";
-	ss << colA << transistors.get_num_mpz_t() << ' ' << colB << plural;
+	integer transistorsInt = toInt(transistors*precision);
+
+	number finalTransistors = number(transistorsInt, precision);
+	str plural = finalTransistors == 1 ? "transistor" : "transistors";
+	ss << colA << finalTransistors << ' ' << colB << plural;
 	return ss.str();
 }
 
@@ -110,14 +116,34 @@ number calcCursorYeild() {
 	return save.cusors * cursorYeild;
 }
 
+number calcMossYeild() {
+	return save.moss * mossYeild;
+}
+
+number calcSmallFABYeild() {
+	return save.smallFABs * smallFABYeild;
+}
+
+number calcMediumFABYeild() {
+	return save.mediumFABs * mediumFABYeild;
+}
+
+number calcLargeFABYeild() {
+	return save.largeFABs * largeFABYeild;
+}
+
 number calcTPS() {
 	number TPS = 0;
 	TPS += calcCursorYeild();
+	TPS += calcMossYeild();
+	TPS += calcSmallFABYeild();
+	TPS += calcMediumFABYeild();
+	TPS += calcLargeFABYeild();
 
 	return TPS;
 }
 
-void onTick(std::vector<str>&) {
+void onTick() {
 	number TPS = calcTPS();
 	increaseTransistors(TPS / 16);
 }
@@ -137,34 +163,78 @@ void click() {
 void balance(std::vector<str>& args) {
 	CMD::log("Ran \"balance\" command");
 	
-	if(args.size() >= 3) {
+	if(args.size() >= 2) {
 		if(args[0] == "building") {
 			integer count;
+			number yeild;
 			number producing;
 			number TPS = calcTPS();
+			number basePrice;
 			str buildingName;
 
-			switch(hash(tolower(args[1]))) {
+			args[1] = tolower(args[1]);
+
+			if(args[1] == "i860" || args[1] == "startup" || args[1] == "oaktree") {
+				std::cout << "Coming soon!\n";
+				return;
+			}
+
+			switch(hash(args[1])) {
 				case cursor:
 					buildingName = "cursor";
 				    count = save.cusors;
 				    producing = calcCursorYeild();
+					basePrice = cursorPrice;
+				    break;
+				case moss:
+				    buildingName = "moss";
+				    count = save.moss;
+				    producing = calcMossYeild();
+					basePrice = mossPrice;
+				    break;
+				case smallFAB:
+				    buildingName = "small FAB";
+				    count = save.smallFABs;
+				    producing = calcSmallFABYeild();
+					basePrice = smallFABPrice;
+				    break;
+				case mediumFAB:
+				    buildingName = "medium FAB";
+				    count = save.mediumFABs;
+				    producing = calcMediumFABYeild();
+					basePrice = mediumFABprice;
+				    break;
+				case largeFAB:
+				    buildingName = "large FAB";
+				    count = save.largeFABs;
+				    producing = calcLargeFABYeild();
+					basePrice = largeFABPrice;
 				    break;
 				default:
 				    std::cout << BOLDRED << "Unknown building!\n";
 				    return;
 			}
 
-		    integer thousandthsOfTPS = toInt((producing / TPS) * 1000);
+			if(count > 0) {
+			yeild = producing / count;
+				
+		    integer thousandthsOfTPS = toInt((yeild * count / TPS) * 1000);
 			number percent = thousandthsOfTPS / 10;
 
-			std::cout << BOLDBLUE << "You have " << buildingName << "s,\n";
-			std::cout << "which poduce " << TransitorsString(producing) << " per second which is " << BOLDGREEN << percent << "%" << BOLDBLUE << " of your total TPS.\n";
+			std::cout << BOLDBLUE << "You have " << BOLDGREEN << count << BOLDBLUE << ' ' << buildingName << "s, each producing " << TransitorsString(yeild) << " per second.\n";
+			std::cout << "which produces " << TransitorsString(producing) << " per second in total which accounts for " << BOLDGREEN << percent << "%" << BOLDBLUE << " of your total TPS.\n";
+			std::cout << "One " << buildingName << " would cost " << TransitorsString(expandPrice(basePrice, count)) << ".\n";
+			return;
+			} else {
+				std::cout << BOLDBLUE << "You have no " << buildingName << "s.\n";
+				std::cout << "One " << buildingName << " would cost " << TransitorsString(expandPrice(basePrice, count)) << ".\n";
+				return;
+			}
 		}
 	}
-	std::cout << BOLDBLUE << "You have " << TransitorsString(save.transistorBalance) << '\n';
-	std::cout << BOLDBLUE << "You have made " << TransitorsString(save.transistorBalance) << " in total!\n";
-	std::cout << BOLDBLUE << "You make" << TransitorsString(calcTPS()) << " per second!\n";
+	std::cout << BOLDBLUE << "You have " << TransitorsString(save.transistorBalance) << ".\n";
+	std::cout << BOLDBLUE << "You have made " << TransitorsString(save.totalTransistors) << " in total.\n";
+	std::cout << BOLDBLUE << "You make " << TransitorsString(calcTPS(), BOLDGREEN, BOLDBLUE, 10) << " per second.\n";
 
 	CMD::log("They have" + TransitorsString(save.transistorBalance, "", "") + "!");
 }
@@ -174,21 +244,45 @@ void buy(std::vector<str>& args) {
 		std::cout << BOLDRED << "You must specify somthing to buy!\n";
 		return;
 	}
-	str building = args[0];
+	str building = tolower(args[0]);
+
+	if(building == "i860" || building == "startup" || building == "oaktree") {
+		std::cout << "coming soon!\n";
+	}
 
 	str buildingName;
 	number basePrice;
 	integer* countPtr = nullptr;
 
-	switch(hash(tolower(building))) {
+	switch(hash(building)) {
 		case cursor:
 		    buildingName = "cursor";
 		    basePrice = cursorPrice;
 			countPtr = &save.cusors;
 		    break;
+		case moss:
+		    buildingName = "moss";
+		    basePrice = mossPrice;
+		    countPtr = &save.moss;
+		    break;
+		case smallFAB:
+		    buildingName = "small FAB";
+		    basePrice = smallFABPrice;
+		    countPtr = &save.smallFABs;
+		    break;
+		case mediumFAB:
+		    buildingName = "medium FAB";
+		    basePrice = mediumFABprice;
+		    countPtr = &save.mediumFABs;
+		    break;
+		case largeFAB:
+		    buildingName = "large FAB";
+		    basePrice = largeFABPrice;
+		    countPtr = &save.largeFABs;
+		    break;
 		default:
 		    std::cout << BOLDRED << "Unknown building!\n";
-		    break;
+			return;
 	}
 
 	integer& count = *countPtr;
@@ -245,9 +339,16 @@ void buy(std::vector<str>& args) {
 }
 
 int main() {
-	std::jthread gameThread = CMD::init(name, GREEN + str("@HCC") + BOLDBLUE + " ~/You" + RESET + "$ ");
+	std::jthread gameThread = CMD::init(name, GREEN + str("@HCC") + BOLDBLUE + " ~/You" + RESET + "$ ", onTick);
 
 	CMD::exit = onExit;
+
+	/*str buildings[] = {"cursor", "moss", "smallFAB", "mediumFAB", "largeFAB", "i860", "startup", "oak tree"};
+	for(int i = 0; i < 8; i++) {
+		std::stringstream ss;
+		ss << buildings[i] << ":" << hash(tolower(buildings[i]));
+		CMD::log(ss.str());
+	}*/
 
 	CMD::addcommand("balance", balance);
 	CMD::addcommand("clear",  clear);
